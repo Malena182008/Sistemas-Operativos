6dd36c406f732c1b42587240fd7ca962b05fe5f1
Malena182008 <168199192+Malena182008@users.noreply.github.com>
2025-04-14 10:56:27 -0300

    mm

---
+++ EjN3.py
@@ -0,0 +1,11 @@
+do = 2.82
+eu = 3.84
+
+print("Ingrese la cantidad de soles: ")
+s = float(input())
+
+ds = s/do
+es = s/eu
+
+print("Con", s,"soles hay", es, "euros")
+print("Con", s,"soles hay", ds, "dólares")
---
+++ EjN5.py
@@ -0,0 +1,7 @@
+print("Ingrese costo unitario del artículo: ")
+c = float(input())
+print("Ingrese costo unitario de docenas: ")
+d = float(input())
+
+p = d*12*c
+print("El precio del artículo es: ", p)
